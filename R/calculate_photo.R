999c3f8979186f4a2e9c6f37242546c97f8233be
Joseph Ronald Stinziano <josephstinziano@gmail.com>
2020-01-06 14:22:42 -0700

    Update calculate_photo.R

--- R/calculate_photo.R
+++ R/calculate_photo.R
@@ -8,32 +8,32 @@ calculate_photo <- function(data_phys, #physiology dataframe
                                             "Oi",
                                             "RH",
                                             "Temp"),
-                            gbw,
                             g0,
                             g1
                             ){
-  #Calculate biological parameters based on temperature
-  Jmax_medlyn <- Arrh_medlyn(k25 = Jmax25,
-                             delS = Jmax_dS,
-                             Ea = Jmax_Ea,
-                             Hd = Jmax_Hd,
-                             Temp = Temp)
   
-  Jmax_new <- Arrh_new(k25 = Jmax25,
-                       delS = Jmax_dS,
-                       Ea = Jmax_Ea,
-                       Hd = Jmax_Hd,
-                       Temp = Temp)
+  A_net <- min(
+    Vcmax * (Ci - GammaStar) / (Ci + GammaStar) - r_day,
+    Jmax 
+  )
   
-  Vcmax_medlyn <- Arrh_medlyn(k25 = Vcmax25,
-                              delS = Vcmax_dS,
-                              Ea = Vcmax_Ea,
-                              Hd = Vcmax_Hd,
-                              Temp = Temp)
+  #Pseudo code below for calculations
+  #Timestep calculations
+  A_gross = ifelse(Qinc > 0, min(Wc, Wj), 0)
   
-  Vcmax_new <- Arrh_new(k25 = Vcmax25,
-                        delS = Vcmax_dS,
-                        Ea = Vcmax_Ea,
-                        Hd = Vcmax_Hd,
-                        Temp = Temp)
+  R = ifelse(Qinc > 0, - r_day, - r_dark)
+  
+  A_net_leaf = A_gross - R
+  
+  A_net_plant = A_net - r_root - r_shoot
+  
+  #Daily sums
+  daily_C_plant = sum(A_net_plant * time_step)
+  
+  daily_photosynthesis = sum(A_gross * time_step)
+  
+  daily_respiration = sum(r_total * time_step)
+  
+  #Output into 2-element list with daily total and hourly values
+  
 }
